424b98768fa19be98f267240e09770dd287f40b9
AbubakarImam <salamabubakarimam@gmail.com>
2022-09-21 16:59:12 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      colors: {
+        veryDarkBlue: 'hsl(238, 29%, 16%)',
+        veryDarkGrayishBlue: 'hsl(237, 12%, 33%)',
+        darkGrayishBlue: 'hsl(240, 6%, 50%)',
+        softRed: 'hsl(14, 88%, 65%)',
+        softViolet: 'hsl(273, 75%, 66%)',
+        softBlue: 'hsl(240, 73%, 65%)',
+        lightGrayishBlue: 'hsl(240, 5%, 91%)'
+      },
+      fontFamily: {
+        'Kumbh Sans': ['Kumbh', 'sans']
+      },
+      backgroundImage: {
+        'desktopBackground': "url('/images/bg-pattern-desktop.svg')",
+        'mobileBackground': "url('/images/bg-pattern-mobile.svg')" 
+      }
+    },
+  },
+  plugins: [],
+}
