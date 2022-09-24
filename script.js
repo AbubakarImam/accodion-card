1896e69de8903b44897379ed501cdd0b3e966bc7
AbubakarImam <salamabubakarimam@gmail.com>
2022-09-24 10:58:29 +0100

    third commit

--- script.js
+++ script.js
@@ -2,9 +2,8 @@ let mainText = document.querySelector(".mText");
 let addText = document.querySelector(".aText");
 let downBtn = document.querySelector(".btn");
 console.log(downBtn)
-downBtn.addEventListener('click', ()=>{
+function show() {
     for(let i = 0; i < addText.length; i++) {
-        addText[i].classList.toggle("hidden")
-    };
-})
-console.log(mainText.innerHTML);
+            addText[i].classList.toggle(".hidden")
+    }
+}
